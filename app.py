9160e596f78a6a7cfab76aea20155aef24983702
emadkhier <emadkh69@gmail.com>
2026-04-03 22:30:41 +0300

    Correct variable name and update return message
    Fixed variable name and improved return message.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__) # תיקון: __name__ במקום _name_
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins + Docker + ECR!111 "
+    return "Hello, Jenkins + Docker + ECR!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__": # תיקון: __name__ ו-"__main__"
+    app.run(host="0.0.0.0", port=5000)
