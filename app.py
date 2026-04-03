f6850627b93e2d923a83a976094d3d824355e9d5
emadkhier <emadkh69@gmail.com>
2026-04-03 17:58:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins + Docker + ECR!11 "
+    return "Hello, Jenkins + Docker + ECR!1 "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
