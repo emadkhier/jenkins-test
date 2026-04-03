b708ed0d8ad6cf4da96f1c5591839e9fbe86cf70
emadkhier <emadkh69@gmail.com>
2026-04-03 17:27:15 +0300

    Update greeting message in home route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins + Docker + ECR!trigger test"
+    return "Hello, Jenkins + Docker + ECR!trigger "
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
