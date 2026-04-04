2849ceed9254e9bf5b1e70b3323c04f4ef3dc2cd
emadkhier <emadkh69@gmail.com>
2026-04-05 00:27:18 +0300

    Fix greeting message in home route
    Updated greeting message in home route.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__) # תיקון: __name__ במקום _name_
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins + Docker + ECR!11"
+    return "Hello, Jenkins + Docker + ECR!111"
 
 if __name__ == "__main__": # תיקון: __name__ ו-"__main__"
     app.run(host="0.0.0.0", port=5000)
